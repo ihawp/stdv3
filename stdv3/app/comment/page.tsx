e50513b9d8f4dab0971cd890e7a05b29c5316a52
ihawp <ihawpebay@gmail.com>
2024-12-19 20:41:44 -0600

    init commit

---
+++ stdv3/app/comment/page.tsx
@@ -0,0 +1,5 @@
+export default function Comment() {
+    return (
+        <h1>COMMENT</h1>
+    )
+}
---
+++ stdv3/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>this is some test for the page</h1>
+    )
+}
