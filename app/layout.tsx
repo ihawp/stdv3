128becaf079873793611642cc213bcf35538427c
ihawp <ihawpebay@gmail.com>
2025-01-02 11:21:42 -0600

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Link from 'next/link';
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <header>
+          <nav className={"flex gap-3"}>
+              <Link href={"/"}>home</Link>
+              <Link href={"/comment"}>comment</Link>
+              <Link href={"/leaderboard"}>leaderboard</Link>
+              <Link href={"/notifications"}>notifications</Link>
+              <Link href={"/profile"}>profile</Link>
+              <Link href={"/settings"}>settings</Link>
+          </nav>
+      </header>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Link from 'next';
+
 export default function Page() {
     return (
-        <h1>this is some test for the page</h1>
+        <>
+            <h1>this is some test for the page</h1>
+        </>
     )
 }
